f44196f400a84d5166ca010d1ccd1feb11dc9401
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-12-31 09:48:29 +0530

    update audit log script

--- Audit Table Scripts/Audit Failure On Notebook.sql
+++ Audit Table Scripts/Audit Failure On Notebook.sql
@@ -1,24 +1,42 @@
 SELECT 
-    NEWID() AS Id,                                    -- Generate unique ID
-    '@{pipeline().RunId}' AS Process_Id,              -- Pipeline run ID
-    '@{pipeline().Pipeline}' AS Process_Name,         -- Pipeline name
-    CASE 
-        WHEN '@{activity('Test Note Book').Status}' = 'Failed' THEN 'Databricks Notebook'
-        ELSE 'Unknown'
-    END AS Activity_Name,                             -- Name of the failed activity
+    -- Generate unique ID
+    NEWID() AS Id,     
+
+    -- Pipeline run ID                       
+    '@{pipeline().RunId}' AS Process_Id, 
+
+    -- Pipeline name             
+    '@{pipeline().Pipeline}' AS Process_Name,
+
+    -- Name of the activity       
+    'Test Copy Data' AS Activity_Name,                
     
-    '@{pipeline().TriggerTime}' AS Start_Time,        -- Pipeline start time
-    '@{utcNow()}' AS End_Time,                        -- Pipeline end time
+    -- Pipeline start time
+    '@{pipeline().TriggerTime}' AS Start_Time,
+
+    -- Pipeline end time        
+    '@{utcNow()}' AS End_Time, 
+
+    -- Execution status                        
     CASE 
-        WHEN '@{activity('Test Note Book').Status}' = 'Failed' THEN 'Failure'
+        WHEN '@{activity('Test Copy Data').Status}' = 'Failed' THEN 'Failure'
         ELSE 'Success'
-    END AS Status,                                    -- Execution status
+    END AS Status,   
+    
+    -- Error code of the failed activity (NULL for success)                                
     CASE 
-        WHEN '@{activity('Test Note Book').Status}' = 'Failed' THEN '@{activity('Test Note Book').Error.errorCode}'
+        WHEN '@{activity('Test Copy Data').Status}' = 'Failed' THEN 
+            '@{if(not(equals(activity('Test Copy Data').Error, null)), activity('Test Copy Data').Error.errorCode, '')}'
         ELSE NULL
-    END AS Error_Code,                                -- Error code of the failed activity
+    END AS Error_Code, 
+
+     
+    -- Error message (success message for successful execution)                              
     CASE 
-        WHEN '@{activity('Test Note Book').Status}' = 'Failed' THEN '@{replace(activity('Test Note Book').Error.message,'''','')}'
+        WHEN '@{activity('Test Copy Data').Status}' = 'Failed' THEN 
+            '@{if(not(equals(activity('Test Copy Data').Error, null)), replace(activity('Test Copy Data').Error.message, '''', ''), '')}'
         ELSE 'Execution successful'
-    END AS Error_Message,                             -- Error message
-    '@{utcNow()}' AS Log_Time                         -- Log timestamp
+    END AS Error_Message,  
+
+    -- Log timestamp                           
+    '@{utcNow()}' AS Log_Time                         
